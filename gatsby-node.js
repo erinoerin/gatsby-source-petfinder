c505b912deb209c02e7b4742c5b17b10d73eaf57
Erin Olmon <42361525+erinoerin@users.noreply.github.com>
2020-07-14 22:14:32 -0700

    Added GraphQL scheme for pet to reduce undefined errors

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,89 @@ exports.sourceNodes = async (
   { actions, createNodeId, createContentDigest },
   options
 ) => {
+  /* Setup the GraphQL Schema for Pets */
+  const { createTypes } = actions;
+  const typeDefs = `
+    type petBreed {
+      primary: String
+      secondary: String
+      mixed: Boolean 
+      unknown: Boolean
+    }
+
+    type petColors {
+      primary: String
+      secondary: String
+      tertiary: String
+    }
+
+    type petPhotos {
+      small: String
+      medium: String
+      large: String
+      full: String
+    }
+
+    type petVideos {
+      embed: String
+    }
+
+    type petAttributes {
+      spayed_neutered: Boolean
+      house_trained: Boolean
+      declawed: Boolean
+      special_needs: Boolean
+      shots_current: Boolean
+    }
+
+    type petEnvironment {
+      children: Boolean
+      dogs: Boolean
+      cats: Boolean
+    }
+    type petAddress{
+      address1: String
+      address2: String
+      city: String
+      state: String
+      postcode: String
+      country: String
+    }
+
+    type petContact {
+      email: String
+      phone: String
+      address: petAddress
+    }
+
+    type PetfinderAnimals implements Node{
+      id: Integer
+      organization_id: String
+      url: String
+      type: String
+      species: String
+      breeds: petBreed
+      colors: petColors
+      age: String
+      gender: String
+      size: String
+      coat: String
+      name: String
+      description: String
+      primary_photo_cropped: petPhotos
+      photos: [petPhotos]
+      videos: [petVideos]
+      status: String
+      attributes: petAttributes
+      environment: petEnvironment
+      tags: [String]
+      contact: petContact
+      published_at: String
+      distance: String
+    }
+  `;
+  createTypes(typeDefs);
+
   const { apiKey, apiSecret, orgID } = options;
 
   const authKey = await getAuthKey(apiKey, apiSecret);
